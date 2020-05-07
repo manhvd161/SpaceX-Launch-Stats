70f07d2bf1578d436a60f1161a12230c929584dc
Sieusaosieungo <thinhhanh1@gmail.com>
2020-05-07 20:22:57 +0700

    fullstack app with graphql

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema');
+
+const app = express();
+
+// Allow cros-origin
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true, // graphical tool in browser
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
